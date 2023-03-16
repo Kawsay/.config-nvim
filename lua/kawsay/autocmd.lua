4c30fdeed2606e4f653efb353fff547c3abc81da
Clément COQUILLE <clement.coquille@happydemics.com>
2023-03-16 13:33:51 +0100

    add autocmd to remove trailing spaces

---
+++ lua/kawsay/autocmd.lua
@@ -0,0 +1,9 @@
+-- Automatically remove trailing spaces when saving a file.
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = {"*"},
+    callback = function(ev)
+        save_cursor = vim.fn.getpos(".")
+        vim.cmd([[%s/\s\+$//e]])
+        vim.fn.setpos(".", save_cursor)
+    end,
+})
